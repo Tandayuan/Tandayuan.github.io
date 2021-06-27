bb86b9538ee1690d7958de5c7079b70c83a95df8
TDY <1293651311@qq.com>
2021-06-27 23:41:38 +0800

    Site updated: 2021-06-27 23:41:38

--- dist/music.js
+++ dist/music.js
@@ -7,12 +7,6 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
-        },
-        {
             name: '逆战',
             artist: '张杰',
             url: 'https://www.ytmp3.cn/down/75143.mp3',
