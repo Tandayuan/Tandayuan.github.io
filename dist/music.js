ec6debad09ae9fd72b7df8b039ae517b740b8021
TDY <1293651311@qq.com>
2021-06-27 23:04:19 +0800

    Site updated: 2021-06-27 23:04:19

--- dist/music.js
+++ dist/music.js
@@ -13,9 +13,9 @@ const ap = new APlayer({
             cover: 'cover1.jpg',
         },
         {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
+            name: '逆战',
+            artist: '张杰',
+            url: 'https://www.ytmp3.cn/down/75143.mp3',
             cover: 'cover2.jpg',
         }
     ]
